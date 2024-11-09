0a9b4947cbd2bbdabafbcfbaa85f62b0db025404
Devenlee250 <devenleekudzai@gmail.com>
2024-11-09 12:57:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '263771600813';
+const defaultOwner = '263776447592';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -23,7 +23,7 @@ global.botname = process.env.BOTNAME || 'ULTRA-MD';
 global.botNumber = '263771600813' //replace your bot number here
 global.mods = []
 global.prems = []
-global.allowed = ['263771600813', '263719765023']
+global.allowed = ['263776447592','263771600813', '263719765023']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
